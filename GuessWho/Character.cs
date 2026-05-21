ee502af00774c78d9954d783cec4748bdb8a5509
Harley Fletcher <flethj1@student.op.ac.nz>
2026-05-21 14:33:10 +1200

    committing again, didn't save work last time

--- GuessWho/Character.cs
+++ GuessWho/Character.cs
@@ -1,8 +1,24 @@
 ﻿using System;
 
-public class Class1
+public class Character
 {
-	public Class1()
+
+	public string Name { get; set; }
+    public Image Placeholder {  get; set; }
+    public Dictionary<string, string> Attributes { get; set; }
+    public bool Hat {  get; set; }
+    public string EyeColour {  get; set; }
+	
+	public Character(string name, Image placeholder, Dictionary<string, string> attributes, bool hat, string eyeColour)
 	{
-	}
+		this.Name = name;
+		this.Placeholder = placeholder;
+		this.Attributes = attributes;
+		this.Hat = hat;
+		this.EyeColour = eyeColour;
+    }
+
+	
+
+
 }
